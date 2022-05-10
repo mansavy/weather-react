e042835343b9a65a5ae62eafbd1add693ea833c0
mansavy <100310641+mansavy@users.noreply.github.com>
2022-05-10 14:05:49 -0300

    Added f conversion to main weather box

--- src/MainTemp.js
+++ src/MainTemp.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "./MainTemp.css";
 import FormatDate from "./FormatDate.js";
 import Icon from "./Icon.js";
-import WeatherTemperature from "./WeatherTemperature.js";
 import LocalTime from "./LocalTime.js";
 import Date from "./Date.js";
 
 export default function MainTemp(props) {
-  const [unit, setUnit] = useState("celsius");
+  function convertF(event) {
+    event.preventDefault();
+    props.setUnit("fahrenheit");
+  }
+
+  function convertC(event) {
+    event.preventDefault();
+    props.setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.data.temperature * 9) / 5 + 32;
+  }
 
   function feels() {
     let feels = Math.round(props.data.feels);
@@ -27,51 +38,122 @@ export default function MainTemp(props) {
     return `${wind} km/h`;
   }
 
-  return (
-    <div className="card-body">
-      <div className="container">
-        <div className="row">
-          <div className="col-5 weather-box">
-            <ul>
-              <li className="stattitle">
-                <Date date={props.data.date} />{" "}
-              </li>
-              <li className="stats mt-0">Feels like: {feels()}</li>
-              <li className="stats">Humidity: {props.data.humidity}%</li>
-              <li className="stats">Wind speed: {wind()}</li>
-            </ul>
-          </div>
-          <div className="col-1"></div>
-
-          <div className="col-5 main-weather">
-            <h2>
-              <span>{props.data.name},</span>
-              <span> {props.data.country} </span>
-            </h2>
-            <div className="timeDescription text-capitalize">
-              <span>
-                <LocalTime date={props.data.date} zone={props.data.timezone} />{" "}
-              </span>
-              |{" "}
-              <span className="text-capitalize"> {props.data.description}</span>{" "}
+  if (props.unit === "celsius") {
+    return (
+      <div className="card-body">
+        <div className="container">
+          <div className="row">
+            <div className="col-5 weather-box">
+              <ul>
+                <li className="stattitle">
+                  <Date date={props.data.date} />{" "}
+                </li>
+                <li className="stats mt-0">Feels like: {feels()}</li>
+                <li className="stats">Humidity: {props.data.humidity}%</li>
+                <li className="stats">Wind speed: {wind()}</li>
+              </ul>
             </div>
-            <span>
-              <Icon code={props.data.icon} />{" "}
-              <WeatherTemperature
-                celsius={props.data.temperature}
-                unit={unit}
-                setUnit={setUnit}
-              />
-            </span>
-            <br />
-            <p>
-              <small>
-                Last updated: <FormatDate date={props.data.date} />
-              </small>
-            </p>
+            <div className="col-1"></div>
+
+            <div className="col-5 main-weather">
+              <h2>
+                <span>{props.data.name},</span>
+                <span> {props.data.country} </span>
+              </h2>
+              <div className="timeDescription text-capitalize">
+                <span>
+                  <LocalTime
+                    date={props.data.date}
+                    zone={props.data.timezone}
+                  />{" "}
+                </span>
+                |{" "}
+                <span className="text-capitalize">
+                  {" "}
+                  {props.data.description}
+                </span>{" "}
+              </div>
+              <span>
+                <Icon code={props.data.icon} />{" "}
+                <span className="temperature">
+                  {" "}
+                  {Math.round(props.data.temperature)}
+                </span>
+                <span className="degree">
+                  °C |{" "}
+                  <a href="/" onClick={convertF}>
+                    {" "}
+                    °F{" "}
+                  </a>
+                </span>
+              </span>
+              <br />
+              <p>
+                <small>
+                  Last updated: <FormatDate date={props.data.date} />
+                </small>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="card-body">
+        <div className="container">
+          <div className="row">
+            <div className="col-5 weather-box">
+              <ul>
+                <li className="stattitle">
+                  <Date date={props.data.date} />{" "}
+                </li>
+                <li className="stats mt-0">Feels like: {feels()}</li>
+                <li className="stats">Humidity: {props.data.humidity}%</li>
+                <li className="stats">Wind speed: {wind()}</li>
+              </ul>
+            </div>
+            <div className="col-1"></div>
+
+            <div className="col-5 main-weather">
+              <h2>
+                <span>{props.data.name},</span>
+                <span> {props.data.country} </span>
+              </h2>
+              <div className="timeDescription text-capitalize">
+                <span>
+                  <LocalTime
+                    date={props.data.date}
+                    zone={props.data.timezone}
+                  />{" "}
+                </span>
+                |{" "}
+                <span className="text-capitalize">
+                  {" "}
+                  {props.data.description}
+                </span>{" "}
+              </div>
+              <span>
+                <Icon code={props.data.icon} />{" "}
+                <span className="temperature"> {Math.round(fahrenheit())}</span>
+                <span className="degree">
+                  <a href="/" onClick={convertC}>
+                    {" "}
+                    °C{" "}
+                  </a>
+                  | °F{" "}
+                </span>
+              </span>
+              <br />
+              <p>
+                <small>
+                  Last updated: <FormatDate date={props.data.date} />
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
